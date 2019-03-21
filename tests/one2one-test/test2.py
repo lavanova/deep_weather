344346be19bc696f9caa506d54fc054e9d7555e5
Wangsitu98 <georgcjf@gmail.com>
2019-03-21 20:26:22 +0100

    deleted irrelevant files

--- tests/one2one-test/test2.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python
-import tensorflow as tf
-import numpy as np
-class why:
-	x = tf.placeholder(tf.float32, shape=(None, 30, 30, 42))
-	rand_array = np.random.rand(32, 30, 30, 42)
-	
