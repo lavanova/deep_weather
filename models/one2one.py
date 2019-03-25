613a4e016408c812852c4d67d6e0bbd6a2cd2424
Chengyuan <icebergmatt0109@gmail.com>
2019-03-25 23:53:09 +0100

    implemented class inheritance
    base_model class is implemented as the parent class for other models

--- models/one2one.py
+++ models/one2one.py
@@ -4,104 +4,38 @@ import random
 import numpy as np
 import os.path as osp
 from utils import variable_summaries
+from utils import base_model
 
 seed = random.randint(1, 1000)
 
+# define model parameter as macros here:
 X_SHAPE = [30, 30, 42]
 
-class One2One(object):
+
+class One2One(base_model):
     def __init__(self, sess, FLAGS):
-        self.data = None
-        self.labels = None
-        self.x = None
-        self.out = None
-        self.y = None
-        self.loss = None
-        self.global_step = None
-        self.train_op = None
-        self.summary_op = None
-
-        self.latest_model = None
-        self.saver = None
-        self.sess = sess
-        self.logger = None
-        self.FLAGS = FLAGS
-        self._buildNet()
-
-    def get_filter(self, shape, name):
-        init = tf.contrib.layers.xavier_initializer(seed = seed)
-        filter = tf.get_variable(name = name + 'weights', regularizer = tf.contrib.layers.l2_regularizer(scale=1.0) ,shape = shape, initializer = init, trainable = True)
-        return filter
+        base_model.__init__(self, sess, FLAGS)
 
     def one2oneLayer(self, input, namespace):
         with tf.variable_scope(namespace):
             shape = X_SHAPE
             weights = tf.Variable(tf.ones(shape), name = 'weights')
-            variable_summaries(weights)
+            variable_summaries(weights, name = 'weights')
             assert weights.graph is tf.get_default_graph()
             bias = tf.Variable(tf.zeros(shape), name = 'bias')
-            variable_summaries(bias)
-        return tf.multiply(input, weights) + bias
+            variable_summaries(bias, name = 'bias')
+            out = tf.multiply(input, weights) + bias
+        return out
 
-    def _buildNet(self):
+    def _buildnet(self):
         with tf.variable_scope('one2one'):
             self.y = tf.placeholder(tf.float32, shape=(None, 30, 30, 42), name='Y')
             self.x = tf.placeholder(tf.float32, shape=(None, 30, 30, 42), name='X')
             l1 = self.one2oneLayer(self.x, "l1")
-            self.out = l1
-            self.loss = tf.losses.mean_squared_error(self.y, self.out)
+            self.loss = tf.losses.mean_squared_error(self.y, l1)
             tf.summary.scalar('MSE loss', self.loss)
             self.global_step = tf.Variable(1, name='global_step', trainable=False)
             self.train_op = tf.train.AdamOptimizer(learning_rate = self.FLAGS.lr).minimize(self.loss, global_step=self.global_step)
             self.summary_op = tf.summary.merge_all()
         self.saver = tf.train.Saver(max_to_keep=30, keep_checkpoint_every_n_hours=6)
         self.logger = tf.summary.FileWriter( osp.join(self.FLAGS.logdir, self.FLAGS.exp), self.sess.graph)
-
-    def _get_loss(self, iter_X, iter_Y):
-        X, Y = self.sess.run([iter_X, iter_Y])
-        dict = {
-            self.x: X,
-            self.y: Y
-        }
-        return self.sess.run(self.loss, feed_dict = dict)
-
-
-    def run(self, iter_data, iter_val = None, train = None, load_path = None):
-        if train == None:
-            train = self.FLAGS.train
-        if load_path == True:
-            self.saver.restore(self.sess, self.latest_model)
-        elif load_path != None:
-            self.saver.restore(self.sess, load_path)
-
-        data_X, data_Y = iter_data.get_next()
-        if iter_val != None:
-            val_X, val_Y = iter_val.get_next()
-
-
-        if train:
-            init = tf.global_variables_initializer()
-            self.sess.run(init)
-            for i in range(self.FLAGS.epoch_num):
-                step = tf.train.global_step(self.sess, self.global_step)
-                X, Y = self.sess.run([data_X, data_Y])
-                train_dict = {
-                    self.x: X,
-                    self.y: Y
-                }
-                summary, _, loss = self.sess.run([self.summary_op, self.train_op, self.loss], feed_dict = train_dict)
-                if i % self.FLAGS.save_interval == 0:
-                    self.latest_model = osp.join(self.FLAGS.ckptdir, self.FLAGS.exp, 'model_{}'.format(i))
-                    self.saver.save(self.sess, self.latest_model)
-                if i % self.FLAGS.log_interval == 0:
-                    self.logger.add_summary(summary, step)
-                    print("epoch " + str(i) + ":")
-                    print("current training loss is: {:.6f}".format(loss))
-                    if iter_val != None:
-                        loss = self._get_loss(val_X, val_Y)
-                        print("current validation loss is: {:.6f}".format(loss))
-
-        else:
-            loss = self._get_loss(data_X, data_Y)
-            print("Testing mode: ")
-            print("The test loss is: {:.6f}".format(loss))
--- models/utils.py
+++ models/utils.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
 import numpy as np
+import os.path as osp
 
 SHAPEX = (12,7,30,30)
 SHAPEY = (6,7,30,30)
@@ -24,14 +25,96 @@ def TF2FLRD(filenames, batchsize=30, buffersize=730, parse=_parse_, oneshot=Fals
     else:
         return train_dataset.make_initializable_iterator()
 
-def variable_summaries(var):
-  """Attach a lot of summaries to a Tensor (for TensorBoard visualization)."""
-  with tf.name_scope('summaries'):
-    mean = tf.reduce_mean(var)
-    tf.summary.scalar('mean', mean)
-    with tf.name_scope('stddev'):
-      stddev = tf.sqrt(tf.reduce_mean(tf.square(var - mean)))
-    tf.summary.scalar('stddev', stddev)
-    tf.summary.scalar('max', tf.reduce_max(var))
-    tf.summary.scalar('min', tf.reduce_min(var))
-    tf.summary.histogram('histogram', var)
+def variable_summaries(var, name=''):
+    """Attach a lot of summaries to a Tensor (for TensorBoard visualization)."""
+    with tf.name_scope('summaries'):
+        with tf.name_scope(name):
+            mean = tf.reduce_mean(var)
+            tf.summary.scalar('mean', mean)
+            with tf.name_scope('stddev'):
+                stddev = tf.sqrt(tf.reduce_mean(tf.square(var - mean)))
+            tf.summary.scalar('stddev', stddev)
+            tf.summary.scalar('max', tf.reduce_max(var))
+            tf.summary.scalar('min', tf.reduce_min(var))
+            tf.summary.histogram('histogram', var)
+
+class base_model():
+    def __init__(self, sess, FLAGS):
+        self.x = None
+        self.y = None
+        self.loss = None
+        self.global_step = None
+        self.train_op = None
+        self.summary_op = None
+
+        self.latest_model = None
+        self.saver = None
+        self.sess = sess
+        self.logger = None
+        self.FLAGS = FLAGS
+        self._buildnet()
+
+    '''
+    building the network in the graph
+    called inside of the class constructor
+    strongly virtual function, requires child class implementation
+    '''
+    def _buildnet(self):
+        raise NotImplementedError()
+
+    '''
+    private function for calculating loss giving iterators
+    '''
+    def _get_loss(self, iter_X, iter_Y):
+        X, Y = self.sess.run([iter_X, iter_Y])
+        dict = {
+            self.x: X,
+            self.y: Y
+        }
+        return self.sess.run(self.loss, feed_dict = dict)
+
+
+    '''
+    run function serves as both training and testing
+    controlled by FLAG.train boolean by default, but changeable by passing boolean into train argument
+    load_path : path to load the ckpt file, if True, then load the latest model recorded
+    '''
+    def run(self, iter_data, iter_val = None, train = None, load_path = None):
+        if train == None:
+            train = self.FLAGS.train
+        if load_path == True:
+            self.saver.restore(self.sess, self.latest_model)
+        elif load_path != None:
+            self.saver.restore(self.sess, load_path)
+
+        data_X, data_Y = iter_data.get_next()
+        if iter_val != None:
+            val_X, val_Y = iter_val.get_next()
+
+        if train:
+            print("Initiating Training mode: ")
+            init = tf.global_variables_initializer()
+            self.sess.run(init)
+            for i in range(self.FLAGS.epoch_num):
+                step = tf.train.global_step(self.sess, self.global_step)
+                X, Y = self.sess.run([data_X, data_Y])
+                train_dict = {
+                    self.x: X,
+                    self.y: Y
+                }
+                summary, _, loss = self.sess.run([self.summary_op, self.train_op, self.loss], feed_dict = train_dict)
+                if i % self.FLAGS.save_interval == 0:
+                    self.latest_model = osp.join(self.FLAGS.ckptdir, self.FLAGS.exp, 'model_{}'.format(i))
+                    self.saver.save(self.sess, self.latest_model)
+                if i % self.FLAGS.log_interval == 0:
+                    self.logger.add_summary(summary, step)
+                    print("epoch " + str(i) + ":")
+                    print("Training loss is: {:.6f}".format(loss))
+                    if iter_val != None:
+                        loss = self._get_loss(val_X, val_Y)
+                        print("Validation loss is: {:.6f}".format(loss))
+
+        else:
+            loss = self._get_loss(data_X, data_Y)
+            print("Initiating Testing mode: ")
+            print("The test loss is: {:.6f}".format(loss))
